2682a5130fc5af775fea78b4384ffd2b53563440
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-07-22 23:39:03 -0300

    test: enhance test for UsersRepository insert_user method with detailed assertions

--- src/infra/db/repositories/test_users_repository.py
+++ src/infra/db/repositories/test_users_repository.py
@@ -1,12 +1,31 @@
 """Test for the Users repository."""
 
+from sqlalchemy import select
+
+from src.infra.db.entities.users import Users
 from src.infra.db.repositories.users_repository import UsersRepository
+from src.infra.db.settings.connection import DBConnectionHandler
 
 
 def test_insert_user() -> None:
 	"""Test the insert_user method of UsersRepository."""
 	first_name = 'Test'
-	last_name = 'User'
-	age = 30
+	last_name = 'Insert'
+	age = 1
 	users_repository = UsersRepository()
 	users_repository.insert_user(first_name, last_name, age)
+
+	with DBConnectionHandler() as database:
+		user = database.session.execute(
+			select(Users).where(
+				Users.first_name == first_name,
+				Users.last_name == last_name,
+				Users.age == age,
+			),
+		).scalar_one_or_none()
+		assert user
+		assert user.first_name == first_name
+		assert user.last_name == last_name
+		assert user.age == age
+		database.session.delete(user)
+		database.session.commit()
